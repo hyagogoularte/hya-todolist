c18a8780a14681798be895aa40cf5502630b97aa
Hyago Goularte <hyagogoularte@gmail.com>
2019-05-03 08:04:01 -0300

    todo items added

---
+++ src/components/todos/items/Items.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Items extends Component {
+  createTasks(item) {
+    return <li key={ item.key }>{ item.content }</li>
+  }
+
+  render() {
+    let todoEntries = this.props.entries;
+    let listItems = todoEntries.map(this.createTasks);
+
+    return (
+        <ul className="todos__list">
+            { listItems }
+        </ul>
+    )
+  }
+}
+
+
+export default Items;
